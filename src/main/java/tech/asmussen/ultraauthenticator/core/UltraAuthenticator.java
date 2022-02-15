039c92406b82548fd6e94772d4bb0b1f46b09d76
Bastian <76102128+BastianAsmussen@users.noreply.github.com>
2022-02-15 13:23:48 +0100

    Add files via upload

---
+++ src/main/java/tech/asmussen/ultraauthenticator/core/UltraAuthenticator.java
@@ -0,0 +1,16 @@
+package tech.asmussen.ultraauthenticator.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltraAuthenticator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
